be73bd64477160ff13492f79921c2e42c992eb7c
rb-0516 <r.erin.brown@gmail.com>
2024-09-26 11:51:57 -0400

    Final Dashboard Update

--- static/js/app.js
+++ static/js/app.js
@@ -3,19 +3,27 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
+    let metadata = data.metadata;
 
 
     // Filter the metadata for the object with the desired sample number
+    let resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    let result = resultArray[0];
 
 
     // Use d3 to select the panel with id of `#sample-metadata`
+    let panel = d3.select("#sample-metadata");
 
 
     // Use `.html("") to clear any existing metadata
+    panel.html("");
 
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
+    Object.entries(result).forEach(([key, value]) => {
+      panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
 
   });
 }
@@ -25,28 +33,69 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
+    let samples = data.samples;
 
 
     // Filter the samples for the object with the desired sample number
-
+    let resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    let result = resultArray[0];
 
     // Get the otu_ids, otu_labels, and sample_values
-
+    let otuIds = result.otu_ids;
+    let otuLabels = result.otu_labels;
+    let sampleValues = result.sample_values;
 
     // Build a Bubble Chart
-
+    var bubbleTrace = {
+      x: otuIds,
+      y: sampleValues,
+      text: otuLabels,
+      mode: 'markers',
+      marker: {
+        color: otuIds,
+        size: sampleValues
+      }
+    };
+    
+    var bubbleLayout = {
+      title: 'Bacteria Cultures per Sample',
+      showlegend: false,
+      height: 600,
+      width: 1200
+    };
+    
 
     // Render the Bubble Chart
+    Plotly.newPlot('bubble', [bubbleTrace], bubbleLayout);
+  
+    // Bar chart
+    let top10OTUs = otuIds.slice(0, 10).reverse();
+    let top10Values = sampleValues.slice(0, 10).reverse();
+    let top10Labels = otuLabels.slice(0, 10).reverse();
 
-
-    // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-
+    // Map the otu_ids to a list of strings for your yticks
+    let yLabels = top10OTUs.map(id => `OTU ${id}`);
 
     // Build a Bar Chart
-    // Don't forget to slice and reverse the input data appropriately
+    var barTrace = {
+      type: 'bar',
+      x: top10Values,
+      y: yLabels, 
+      text: top10Labels, 
+      orientation: 'h'
+    };
 
-
+    var barLayout = {
+      title: 'Top 10 Bacteria Cultures Found',
+      xaxis: { title: 'Sample Values' },
+      yaxis: { title: 'OTU ID' },
+      margin: { l: 100, r: 100, t: 100, b: 100 },
+      height: 500,
+      width: 1000
+    };
+  
     // Render the Bar Chart
+    Plotly.newPlot('bar', [barTrace], barLayout);
 
   });
 }
@@ -56,28 +105,39 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
+    let names = data.names;
 
 
     // Use d3 to select the dropdown with id of `#selDataset`
+    let dropdownMenu = d3.select("#selDataset");
 
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-
-
+    dropdownMenu.selectAll("option")
+      .data(names)
+      .enter()
+      .append("option")
+      .text(d => d)
+      .attr("value", d => d.toLowerCase());
+  
     // Get the first sample from the list
+    let firstSample = names[0];
 
 
     // Build charts and metadata panel with the first sample
-
+    buildMetadata(firstSample);
+    buildCharts(firstSample);
+  
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-
+  buildMetadata(newSample);
+  buildCharts(newSample);
 }
 
 // Initialize the dashboard
